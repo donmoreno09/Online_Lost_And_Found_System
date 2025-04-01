80befcf2caaf0d0ed79f6437962ae65bb1de16b1
Don Moreno <d.moreno@esis-italia.com>
2025-04-01 15:23:35 +0200

    Provato a fare richieste http

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
+import userRouter from './router/users.js'; 
 
 const server = express();
 
 const PORT = process.env.PORT || 5000;
 
 server.use(express.json()); // serve per parsare il body delle richieste in json
+
+server.use(userRouter);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
